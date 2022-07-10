c5a10e6da85461aa72de2605217389ae2ea28bce
Jonathan Felix <jhow.wi56@gmail.com>
2022-07-09 21:12:34 -0300

    add service

--- src/main/java/br/com/soap/mapstruct/EnderecoMapper.java
+++ src/main/java/br/com/soap/mapstruct/EnderecoMapper.java
@@ -1,11 +1,12 @@
 package br.com.soap.mapstruct;
 
 import br.com.correios.bsb.sigep.master.bean.cliente.EnderecoERP;
+
 import br.com.soap.dtos.EnderecoDTO;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "cdi")
-public interface EnderecoStruct {
+public interface EnderecoMapper {
 
 
     EnderecoDTO toDTO(EnderecoERP enderecoERP);
--- src/main/java/br/com/soap/services/ClienteSoapService.java
+++ src/main/java/br/com/soap/services/ClienteSoapService.java
@@ -1,7 +1,13 @@
 package br.com.soap.services;
 
 
+
+import br.com.correios.bsb.sigep.master.bean.cliente.AtendeCliente;
+import br.com.correios.bsb.sigep.master.bean.cliente.SQLException_Exception;
+import br.com.correios.bsb.sigep.master.bean.cliente.SigepClienteException;
 import br.com.soap.cliente.ClienteSoapConnection;
+import br.com.soap.dtos.EnderecoDTO;
+import br.com.soap.mapstruct.EnderecoMapperImpl;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
@@ -10,11 +16,23 @@ import javax.inject.Inject;
 public class ClienteSoapService {
 
 
+    @Inject
+    EnderecoMapperImpl enderecoMapper;
 
     @Inject
     ClienteSoapConnection clienteSoapConnection;
 
 
+    public EnderecoDTO buscaCEP(String cep){
+        try{
+            AtendeCliente cliente = clienteSoapConnection.connectClient();
+            EnderecoDTO enderecoDTO = enderecoMapper.toDTO(cliente.consultaCEP(cep));
+            return enderecoDTO;
+        }catch(SQLException_Exception | SigepClienteException e){
+            throw new RuntimeException(e.getMessage());
+        }
+
+    }
 
 
 
